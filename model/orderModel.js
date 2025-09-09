5660d4a75d5d01fc0bc16108699562d4b4bc2d10
Aaradhya <aaradhyaprnjp62.com>
2025-09-09 15:46:00 +0530

    changes

--- model/orderModel.js
+++ model/orderModel.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Link order to user
   name: String,
   email: String,
   city: String,
   address: String,
   pincode: Number,
   age: Number,
-  contact: String,
+  contact: String, // if you're saving contact info
   products: [String],
-  productID: [String],
-  image: String,
+  productID :[String],
+  image:String,
   amount: String,
-  taskstatus: { type: Boolean, default: false }
+  
+  taskstatus: {type:Boolean,default:false}
 }, { timestamps: true });
 
 module.exports = mongoose.model("Order", orderSchema);
